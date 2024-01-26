7e7b52579c00e6100225df03920e3a2ebc96de5b
Paul <Sivivatu@users.noreply.github.com>
2024-01-26 13:51:46 +0000

    Add film list and partial template for table

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Request
+from typing import Optional
+from fastapi import FastAPI, Request, Header
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
@@ -7,7 +8,16 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-async def root(request: Request):
-    context = {"request": request}
+async def root(request: Request, hx_request: Optional[str] = Header(None)):
+    films = [
+        {'name': 'Blade Runner', 'director': 'Ridley Scott'},
+        {'name': 'Pulp Fiction', 'director': 'Quentin Tarantino'},
+        {'name': 'Mulholland Drive', 'director': 'David Lynch'},
+        {'name': 'The Shawshank Redemption', 'director': 'Frank Darabont'},
+    ]
+    context = {"request": request, "films": films}
+    if hx_request:
+        return templates.TemplateResponse("partial/table.html", context)
+
     return templates.TemplateResponse("index.html", context)
 
