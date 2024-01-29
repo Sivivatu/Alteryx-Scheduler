74237dcf345f7890b645a6d592b928429d675076
Paul <Sivivatu@users.noreply.github.com>
2024-01-29 09:16:04 +0000

    add updated logging to main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import json
 from typing import Annotated, Optional
 from fastapi import Depends, FastAPI, Request, Header
 from fastapi.responses import HTMLResponse
@@ -6,6 +7,16 @@ from fastapi.security import OAuth2PasswordBearer
 
 from database.database import create_db_and_tables, create_heroes, select_heroes
 
+import logging.config
+
+logger = logging.getLogger(__name__)
+log_config_file = "/workspaces/Alteryx-Scheduler/app/app_logger/logging_configs/log_config.json"
+with open(log_config_file) as f_in:
+    config = json.load(f_in)
+    logging.config.dictConfig(config)
+
+
+logger.info("Starting FastAPI app")
 app = FastAPI()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
