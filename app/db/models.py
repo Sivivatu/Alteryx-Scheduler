2cc9ade861e808608c867c3131237242bc457383
Paul <Sivivatu@users.noreply.github.com>
2024-02-11 21:56:46 +0000

    removed unneeded imports & added additional user fields

--- app/db/models.py
+++ app/db/models.py
@@ -1,15 +1,18 @@
-from dataclasses import field
-import os
-from typing import Optional
-
 # One line of FastAPI imports here later 👈
-from sqlmodel import Field, SQLModel, Session, create_engine, select
-
+from sqlmodel import Field, SQLModel
 
 class Users(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+    id: int | None = Field(default=None, primary_key=True)
     username: str
     hashed_password: str
-    alteryx_id: Optional[str]
-    alteryx_api_key: Optional[str]
-    alteryx_api_secret: Optional[str]
+    email: str
+    full_name: str
+    disabled: bool = False
+    alteryx_id: str | None = None
+    alteryx_api_key: str | None = None
+    alteryx_api_secret: str | None = None
+
+class UserInDB(Users):
+    hashed_password: str
+
+# async def get_username(username: str, session: AsyncSession):
