755bfbe2e5455d1d0a2e105217e5ed59c7f80b8d
Paul <Sivivatu@users.noreply.github.com>
2024-03-02 21:24:30 +0000

    added primary colours

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,html}"],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#202225",
+        secondary: "#5865F2",
+      },
+    },
   },
   plugins: [],
 };
