d02043c778c46c5883fe8ebafc601f6203c20fad
Paul <Sivivatu@users.noreply.github.com>
2024-03-05 21:56:53 +0000

    shadcn-ui base example dashboard

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -3,8 +3,20 @@ import { FaFire, FaPoo } from 'react-icons/fa';
 
 const Sidebar = () => {
   return (
-    <div className="fixed left-0 top-0 m-0 flex h-screen w-16 flex-col bg-slate-200 text-black shadow-lg dark:bg-slate-900 dark:text-white">
+    <div className="fixed left-0 top-0 m-0 flex h-screen w-16 flex-col bg-primary text-primeText shadow-lg dark:bg-secondary dark:text-primeText-dark">
       <h1>SideBar</h1>
+
+      <nav className="flex sm:justify-center space-x-4">
+  {[
+    ['Home', '/dashboard'],
+    ['Team', '/team'],
+    ['Projects', '/projects'],
+    ['Reports', '/reports'],
+  ].map(([title, url]) => (
+    <a href={url} className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900">{title}</a>
+  ))}
+</nav>
+      
 
       <SidebarIcon icon={<FaFire size="28" />} />
       <Divider />
