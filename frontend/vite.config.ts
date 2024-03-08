43f06280f6d01335bf6c053db911c22710710cb1
Paul <Sivivatu@users.noreply.github.com>
2024-03-08 22:13:05 +0000

    update config for path alias resolution

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,15 @@
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  plugins: [react(), tsconfigPaths()],
+  // resolve: {
+  //   alias: {
+  //     '@': path.resolve(__dirname, './src'),
+  //   },
+  // },
   server: {
     watch: {
       usePolling: true
